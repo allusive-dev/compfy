5650d887357bf2a3fac8c5fd4f467bf8795b5fc4
allusive-dev <jasper@allusive.dev>
2023-09-19 17:46:20 +1000

    reset

--- src/event.h
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MPL-2.0
-// Copyright (c) 2019, Yuxuan Shui <yshuiv7@gmail.com>
-
-#include <xcb/xcb.h>
-
-#include "common.h"
-
-void ev_handle(session_t *ps, xcb_generic_event_t *ev);
--- src/uthash_extra.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <uthash.h>
-
-#define HASH_ITER2(head, el)                                                             \
-	for (__typeof__(head) el = (head), __tmp = el != NULL ? el->hh.next : NULL;      \
-	     el != NULL; el = __tmp, __tmp = el != NULL ? el->hh.next : NULL)
