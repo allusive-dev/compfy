0672207126c003d630a9248e1b5c8ec1ab0dfe77
IogaMaster <iogamastercode@gmail.com>
2023-11-02 15:07:07 -0600

    flake: Use nixpkgs-unstable and stop using url literals

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
