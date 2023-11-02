88769e82fc80939d61429ea6cbb03057e81a5dcb
IogaMaster <iogamastercode@gmail.com>
2023-11-02 15:00:54 -0600

    Add flake.nix and .gitignore

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ ];
+          buildInputs = with pkgs; [ ];
+        };
+
+        packages.default = pkgs.picom.overrideAttrs (old: { src = ./.; });
+      }
+    );
+}
