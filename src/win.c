7e8a4deccdbdf98749c58f09297089a6df756039
allusive-dev <jasper@allusive.dev>
2023-11-09 12:20:30 +1100

    fix focus wintype rule still applying dimming to certain windows

--- src/win.c
+++ src/win.c
@@ -1222,25 +1222,29 @@ bool win_should_dim(session_t *ps, const struct managed_win *w) {
 		return false;
 	}
 
-	if (ps->o.support_for_wm == WM_SUPPORT_DWM) {
-		if (ps->o.inactive_dim > 0 && !win_is_focused_raw(ps, w)) {
-			if (c2_match(ps, w, ps->o.inactive_opacity_blacklist, NULL)) {
-				return false;
-			} else {
-				return true;
-			}
-		} else {
-			return false;
-		}
+	if (ps->o.wintype_option[w->window_type].focus) {
+		return false;
 	} else {
-		if (ps->o.inactive_dim > 0 && !win_is_focused_raw(ps, w)) {
-			if (c2_match(ps, w, ps->o.inactive_opacity_blacklist, NULL)) {
-				return false;
+		if (ps->o.support_for_wm == WM_SUPPORT_DWM) {
+			if (ps->o.inactive_dim > 0 && !win_is_focused_raw(ps, w)) {
+				if (c2_match(ps, w, ps->o.inactive_opacity_blacklist, NULL)) {
+					return false;
+				} else {
+					return true;
+				}
 			} else {
-				return true;
+				return false;
 			}
 		} else {
-			return false;
+			if (ps->o.inactive_dim > 0 && !win_is_focused_raw(ps, w)) {
+				if (c2_match(ps, w, ps->o.inactive_opacity_blacklist, NULL)) {
+					return false;
+				} else {
+					return true;
+				}
+			} else {
+				return false;
+			}
 		}
 	}
 }
